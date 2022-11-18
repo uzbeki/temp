38372c04910a0ea0d17314a20d9c311fe433c7f7
Beki <uzbekitube@gmail.com>
2022-11-18 11:55:32 +0500

    batch fix

--- block_init_talk_with.bat
+++ block_init_talk_with.bat
@@ -1,7 +1,7 @@
 @echo OFF
 if exist C:\talk_with_block rmdir /s /q C:\talk_with_block
 
-git -C C:\ clone -b block git@github.com:uzbeki/talk_with.git talk_with_block && cd C:\talk_with_block && copy app\.env.sample app\.env.local && notepad app\.env.local
+git -C C:\ clone git@github.com:uzbeki/talk_with.git talk_with_block && cd C:\talk_with_block && git checkout block && copy app\.env.sample app\.env.local && notepad app\.env.local
 
 echo "notepadアプリが開かれました。必要な値を入れ保存し、notepadを閉じてください。閉じたら、こちらのコマンドプロンプトでEnterを押してお待ちください。"
 
